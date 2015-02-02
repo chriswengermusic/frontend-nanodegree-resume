bc48bca1a71c9e568e4613f1cad8d01e2a585541
Chris Wenger <chriswengermusic@gmail.com>
2015-02-02 07:34:22 -0500

    Update objects for resume, clean up extra code

---
+++ js/resumeBuilder.js
@@ -0,0 +1,140 @@
+var bio = {
+	"name": "Christopher M. Wenger",
+	"role": "Music Teacher and budding Front-End Developer",
+	"contacts": {
+	"email": "chriswengermusic@gmail.com",
+	"phone": "717.856.4000",
+	"gitHub": "https://github.com/chriswengermusic",
+	"location": "Central Pennsylvania"
+	},
+	"picture": "http://upload.wikimedia.org/wikipedia/commons/1/13/Cello_bridge2.JPG",
+	"welcome": "Welcome to my online resume.",
+	"skills": ["teaching", "playing", "composing", "conducting", "coding", "web-design"]
+}
+var work = {
+	"positions":[
+	{
+		"employer": "Bobby Rahal Honda",
+		"position": "Sales and Leasing Consultant",
+		"city": "Mechanicsburg, PA",
+		"description": "Provide customers with service to buy and lease new
+			and used automobiles. Arrange the details of the transaction and provide
+			service after the sale.",
+		"dates": "2001-2002"
+	},
+	{
+		"employer": "Carlisle Area School District",
+		"position": "LTS - Middle/High School Orchestra",
+		"city": "Carlisle, PA",
+		"description": "Conduct daily rhearsals for 9-12th grade string orchestra, as
+			well as 6-8th grade lessons and large-group orchestra rehearsals on a weekly
+			basis.",
+		"dates": "2002"
+	},
+	{
+		"employer": "Bible Baptist School",
+		"position": "Band Director (PT)",
+		"city": "Shiremanstown, PA",
+		"description": "Conduct weekly pull-out instrumental lessons for students in 
+			grades 4-12, as well as large group Elementary and Middle-High School Band.",
+		"dates": "2002-2006"
+	},
+	{
+		"employer": "Trinity High School",
+		"position": "String Ensemble/Choir Director (PT)",
+		"city": "Camp Hill,PA",
+		"description": "Schedule and conduct weekly pull-out lessons and orchestra 
+			rehearsals for string players in grades 9-12.",
+		"dates": "2002- 2009"
+	},
+	{
+		"employer": "North Penn School District",
+		"position": "Elementary String Specialist",
+		"city": "Lansdale, PA",
+		"description": "Schedule and conduct twice-weekly pull-out lessons and once-
+			weekly large group orchestra rehearsals for string students in grades 3-6.
+			Arrange and implement Suzuki violin lessons for students/families in grades
+			K-6.",
+		"dates": "2009-2010"
+	},
+	{
+		"employer": "Central Dauphin School District",
+		"position": "Elementary Instrumental Music Teacher",
+		"city": "Harrisburg, PA",
+		"description": "Schedule and conduct weekly pull-out lessons and large group band
+			and orchestra rehearsals for instrumental students in grades 4-5."
+		"dates": "2010-present"
+	}
+	]
+}
+
+var education = {
+	"schools": [
+	{
+		"name": "Messiah College",
+		"city": "Grantham, PA",
+		"degree": "B.A.", 
+		"major": ["Music, concentration in 'Cello performance"],
+		"year": 2001
+	},
+	{
+		"name": "Kutztown University of PA",
+		"city": "Kutztown, PA",
+		"degree": "Instructional I",
+		"major": ["K-12 Music"],
+		"year": 2010
+	},
+	{
+		"name": "Messiah College",
+		"city": "Grantham, PA",
+		"degree": "Instructional II",
+		"major": ["K-12 Music"],
+		"year": 2014
+	}		
+],
+"online courses": [
+	{
+		"title": "How to use Git and GitHub",
+		"school": "Udacity",
+		"dates": 2015,
+		"url": "https://www.udacity.com/course/ud775"
+	},
+	{
+		"title": "JavaScript Basics",
+		"school": "Udacity",
+		"dates": 2015,
+		"url": "https://www.udacity.com/course/ud804"
+	]
+}
+
+var projects = {
+	"sampleWork":[
+	{
+		"name": "Interactive Resume",
+		"gitHub": "https://github.com/chriswengermusic/frontend-nanodegree-resume"
+	}
+	]
+}
+
+/*var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.phone);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.gitHub);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%", bio.welcome);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills); 
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);	
+$("#header").prepend(formattedPic);
+$("#topContacts").append(formattedWelcome);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedGitHub);
+$("#topContacts").append(formattedLocation);
+$("#main").append(HTMLskills);
+//$("#main").append(work["position"]);
+//$("#main").append(education.name);
+*/
+
